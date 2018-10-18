8716c31d9c526e548aa308e33fdd5efa5b170c95
Adi Gonnen <adigonnen@gmail.com>
2018-10-19 00:47:54 +0300

    Updates

---
+++ precache-manifest.add154f1c8b6a542f02c1e06bb46ad5a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "21b4e93da0fe2322c716",
+    "url": "/cinema/static/js/runtime~main.21b4e93d.js"
+  },
+  {
+    "revision": "3773ab701367275d5a37",
+    "url": "/cinema/static/js/main.3773ab70.chunk.js"
+  },
+  {
+    "revision": "163889f462ec10b5c9f2",
+    "url": "/cinema/static/js/1.163889f4.chunk.js"
+  },
+  {
+    "revision": "3773ab701367275d5a37",
+    "url": "/cinema/static/css/main.3660147f.chunk.css"
+  },
+  {
+    "revision": "9371176a0e166121999c35adbf7f8e69",
+    "url": "/cinema/index.html"
+  }
+];
