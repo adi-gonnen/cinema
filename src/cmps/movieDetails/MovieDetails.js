737ee2649aa7574e8d0643ea2ff267bf1e543a31
Adi Gonnen <adigonnen@gmail.com>
2018-10-18 10:58:45 +0300

    fix genre tabs

--- src/cmps/movieDetails/MovieDetails.js
+++ src/cmps/movieDetails/MovieDetails.js
@@ -85,7 +85,7 @@ export default class MovieDetails extends Component {
                                 <p className="runtime"> {movie.Runtime},&nbsp;</p>
                                 <p className="language"> {movie.Language? movie.Language: 'Jibrish'}</p>
                             </div>
-                            <p className="plot">{movie.Plot? movie.Plot: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.'}</p>
+                            <p className="plot">{movie.Plot? movie.Plot: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'}</p>
                         </div>
                     </div>
                     <div className="btns flex">
